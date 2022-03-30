f440cbb1855c8210d64c67cd34f598a3f5dff0c4
Chris Kjolhede <ckjolhede@gmail.com>
2022-03-29 21:52:14 -0600

    test: create validation test for schools

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,4 +2,11 @@
 
 class School < ApplicationRecord
   has_many :students
+
+  validates_presence_of :name
+  validates_presence_of :census
+  
+  def order
+  end
+
 end
--- spec/features/schools/index_spec.rb
+++ spec/features/schools/index_spec.rb
@@ -8,8 +8,8 @@ require 'rails_helper'
 
 RSpec.describe 'schools index page', type: :feature do
   before(:each) do
-    @school_1 = School.create!(name: "City High School", census: 200)
-    @school_2 = School.create!(name: "City Middle School", census: 97)
+    @school_2 = School.create!(name: "City High School", census: 200)
+    @school_1 = School.create!(name: "City Middle School", census: 97)
     @school_3 = School.create!(name: "City Grade School", census: 104)
     @student_1 = Student.create!(name: "Abraham Adams", gpa: 1.2, freelunch: true,school_id: @school_1.id)
     @student_11 = Student.create!(name: "Alicia Arden", gpa: 4.0, freelunch: false, school_id: @school_1.id)
@@ -20,15 +20,6 @@ RSpec.describe 'schools index page', type: :feature do
   end
 
   it 'shows all schools' do
-    @school_1 = School.create!(name: "City High School", census: 200)
-    @school_2 = School.create!(name: "City Middle School", census: 97)
-    @school_3 = School.create!(name: "City Grade School", census: 104)
-    @student_1 = Student.create!(name: "Abraham Adams", gpa: 1.2, freelunch: true,school_id: @school_1.id)
-    @student_11 = Student.create!(name: "Alicia Arden", gpa: 4.0, freelunch: false, school_id: @school_1.id)
-    @student_2 = Student.create!(name: "Bethany Bingham", gpa: 2.3, freelunch: false, school_id: @school_2.id)
-    @student_22 = Student.create!(name: "Bert Bond", gpa: 2.0, freelunch: true, school_id: @school_2.id)
-    @student_3 = Student.create!(name: "Cher CornCob", gpa: 3.4, freelunch: false, school_id: @school_3.id)
-    @student_33 = Student.create!(name: "Carl Cobert", gpa: 2.7, freelunch: true, school_id: @school_3.id)
 
     visit '/schools'
 
@@ -40,5 +31,11 @@ RSpec.describe 'schools index page', type: :feature do
     expect(page).to have_content(@school_3.census)
   end
 
+  skip it 'shows all schools in order of creation' do
 
+    visit '/schools'
+
+  expect(@school_1.name).to appear_after(@school_2.name)
+  expect(@chool_1.name).to appear_before(@school_3.name)
+  end
 end
